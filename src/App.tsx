3a1ecb10fb2224de85f61a21325e1614d0d5b6cd
codenester <srrthea@gmail.com>
2023-01-24 18:02:17 +0700

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 // import { useContext, useEffect, useState } from 'react'
-import { FC } from 'react';
+// import { FC } from 'react';
+import { FC, useState } from 'react'
+import { useSelector } from 'react-redux'
 import './App.css'
 import Header from './components/layouts/headers/Header'
 import { viewSlice, viewStore } from './store'
-import { useDispatch, useSelector, useStore } from 'react-redux';
 function App() {
-  // viewStore.subscribe(() => console.log(viewStore.getState()))
-  // const store = useStore()
-  const selector = useSelector((state: { dialogs: FC<{ name?: string }>[] }) => state.dialogs)
+  const dialogs = useSelector((state: { dialogs: FC<{ name?: string }>[] }) => state.dialogs)
   const { add, remove } = viewSlice.actions;
   return (
     <div className="App">
       <button onClick={() => viewStore.dispatch(add(Header))}>add dialog</button>
       <button onClick={() => viewStore.dispatch(remove())}>remove dialog</button>
-      {selector.map((S, i: number) => (<S key={i} name={`header ${i + 1}`} />))}
+      {dialogs.map((D, i) => (<D key={i} name={`header ${i + 1}`} />))}
     </div>
   )
 }
